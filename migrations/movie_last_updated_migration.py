7745b8c22a4e2552830e536c8d82f2fbbc5da24d
Andri Gunawan <andri_gunawan@msn.com>
2019-07-20 17:01:47 -0700

    Ticket: Migration
    Update all the documents in the `movies` collection, such that the "lastupdated"
    field is stored as an ISODate() rather than a string.
    
    The parser.parse() method can transform date strings into ISODate objects for
    us. We just need to make sure the correct operations are sent to MongoDB!

--- migrations/movie_last_updated_migration.py
+++ migrations/movie_last_updated_migration.py
@@ -3,30 +3,17 @@ from pymongo.errors import InvalidOperation
 from bson import ObjectId
 import dateutil.parser as parser
 
-"""
-Ticket: Migration
-
-Update all the documents in the `movies` collection, such that the "lastupdated"
-field is stored as an ISODate() rather than a string.
-
-The parser.parse() method can transform date strings into ISODate objects for
-us. We just need to make sure the correct operations are sent to MongoDB!
-"""
-
 # ensure you update your host information below!
-host = "mongodb://localhost:27017"
+host = "mongodb+srv://m220student:m220password@mflix-zqxvf.mongodb.net/test?retryWrites=true&w=majority"
 
 # don't update this information
 MFLIX_DB_NAME = "sample_mflix"
 mflix = MongoClient(host)[MFLIX_DB_NAME]
 
-# TODO: Create the proper predicate and projection
-# add a predicate that checks that the "lastupdated" field exists, and then
-# checks that its type is a string
-# a projection is not required, but may help reduce the amount of data sent
-# over the wire!
-predicate = {"some_field": {"$some_operator": "some_expression"}}
-projection = None
+# here we're making sure "lastupdated" exists in the document as a string
+predicate = {"lastupdated": {"$exists": True, "$type": "string"}}
+# this projection only sends the "lastupdated" and "_id" fields back to the client
+projection = {"lastupdated": 1}
 
 cursor = mflix.movies.find(predicate, projection)
 
@@ -45,12 +32,9 @@ for doc in cursor:
 print(f"{len(movies_to_migrate)} documents to migrate")
 
 try:
-    # TODO: Complete the UpdateOne statement below
-    # build the UpdateOne so it updates the "lastupdated" field to contain
-    # the new ISODate() type
     bulk_updates = [UpdateOne(
         {"_id": movie.get("doc_id")},
-        {"$some_update_operator": {"some_field_to_update"}}
+        {"$set": {"lastupdated": movie.get("lastupdated")}}
     ) for movie in movies_to_migrate]
 
     # here's where the bulk operation is sent to MongoDB
